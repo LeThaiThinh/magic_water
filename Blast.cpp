f88277cecb521c47a4b921132acb1ed9a36e9f8a
mr.0332470124 <48116154+LeThaiThinh@users.noreply.github.com>
2019-05-05 08:51:26 +0700

    Update Blast.cpp

--- Blast.cpp
+++ Blast.cpp
@@ -1,4 +1,4 @@
-#include"Blast.h"
+include"Blast.h"
 
 Blast::Blast()
 {
@@ -9,23 +9,21 @@ Blast::Blast()
 	vx = 0;
 	vy = 0;
 	ismove = false;
+	
 }
 Blast::~Blast()
 {
-
+	des.x = 0;
+	des.y = 0;
+	des.w = 0;
+	des.h = 0;
+	vx = 0;
+	vy = 0;
+	ismove = false;
+	ObjTex = NULL;
 }
 
-void Blast::MoveRight(const int& x_border, const int& y_border)
-{
-	if (ismove == true)
-	{
-		des.x += vx;
-		if (des.x > x_border or des.y > y_border or des.x < 0 or des.y < 0)
-		{
-			ismove = false;
-		}
-	}
-}
+
 
 void Blast::render()
 {
@@ -37,9 +35,13 @@ void Blast::MoveLeft(const int& x_border, const int& y_border)
 	if (ismove == true)
 	{
 		des.x -= vx;
-		if (des.x > x_border or des.y > y_border or des.x < 0 or des.y < 0)
+		if (des.x+BlastWidth > x_border or des.y + BlastHeight > y_border or des.x < 0 or des.y < 0)
 		{
 			ismove = false;
+			if (this != NULL)
+			{
+				this->~Blast();
+			}
 		}
 	}
 }
@@ -49,9 +51,13 @@ void Blast::MoveUp(const int& x_border, const int& y_border)
 	if (ismove == true)
 	{
 		des.y -= vy;
-		if (des.x > x_border or des.y>y_border or des.x<0 or des.y<0)
+		if (des.x + BlastWidth> x_border or des.y + BlastHeight >y_border or des.x<0 or des.y<0)
 		{
 			ismove = false;
+			if (this != NULL)
+			{
+				this->~Blast();
+			}
 		}
 	}
 }
@@ -61,11 +67,30 @@ void Blast::MoveDown(const int& x_border, const int& y_border)
 	if (ismove == true)
 	{
 		des.y += vy;
-		if (des.x > x_border or des.y > y_border or des.x < 0 or des.y < 0)
+		if (des.x +BlastWidth> x_border or des.y+BlastHeight > y_border or des.x < 0 or des.y < 0)
 		{
 			ismove = false;
+			if (this != NULL)
+			{
+				this->~Blast();
+			}
 		}
 	}
 }
 
+void Blast::MoveRight(const int& x_border, const int& y_border)
+{
+	if (ismove == true)
+	{
+		des.x += vx;
+		if (des.x+ BlastWidth > x_border or des.y + BlastHeight > y_border or des.x < 0 or des.y < 0)
+		{
+			ismove = false;
+			if (this != NULL)
+			{
+				this->~Blast();
+			}
+		}
+	}
+}
 
